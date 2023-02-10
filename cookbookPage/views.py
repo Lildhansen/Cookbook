6061acfe1e6a6b31cb00984cc38cc759fff1c039
lildhansen <madslh@yahoo.com>
2023-02-11 00:07:06 +0100

    started working on the tags section - some difficulties with the stylesheet

--- cookbookPage/views.py
+++ cookbookPage/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tag #direct from database - you should not to this in the end!
 
 
 def index(request):
     a = 10
-    context = {'a':a}
+    tags = Tag.objects.all()
+    context = {'tags':tags}
     return render(request, 'index.html',context)
